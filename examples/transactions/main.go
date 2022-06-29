04fc8ba95d79ff89bb47dbd3f00f95b3d9569252
apstndb <803393+apstndb@users.noreply.github.com>
2022-06-29 16:17:18 +0900

    Update

--- examples/transactions/main.go
+++ examples/transactions/main.go
@@ -39,12 +39,12 @@ func run(ctx context.Context) error {
 	tx := client.ReadOnlyTransaction([]spannerplus.Tag{{"tx", "ro1"}})
 	defer tx.Close()
 
-	// app=tag-sandbox,tx=ro1,h=1bb5077c
+	// request_tag: app=tag-sandbox,tx=ro1,h=1bb5077c
 	if err = tx.QueryWithHash(ctx, spanner.NewStatement("SELECT 1")).Do(nop); err != nil {
 		return err
 	}
 
-	// app=tag-sandbox,tx=ro1,h=9087fd8b
+	// request_tag: app=tag-sandbox,tx=ro1,h=9087fd8b
 	if err = tx.QueryWithHash(ctx, spanner.NewStatement("SELECT 2")).Do(nop); err != nil {
 		return err
 	}
@@ -53,17 +53,17 @@ func run(ctx context.Context) error {
 		return tx.QueryWithTags(ctx, spanner.NewStatement("SELECT * FROM Singers"), spannerplus.Tag{"query", "FindAllSingers"}).Do(nop)
 	}
 
-	// app=tag-sandbox,tx=ro1,query=FindAllSingers
+	// request_tag: app=tag-sandbox,tx=ro1,query=FindAllSingers
 	if err = FindAllSingers(ctx, tx); err != nil {
 		return err
 	}
 
 	grp, ctx := errgroup.WithContext(ctx)
 	grp.Go(func() error {
-		// app=tag-sandbox,tx=rw1,
+		// transaction_tag: app=tag-sandbox,tx=rw1,
 		_, err = client.ReadWriteTransaction(ctx, []spannerplus.Tag{{"tx", "rw1"}},
 			func(ctx context.Context, transaction *spannerplus.ReadWriteTransactionWrapper) error {
-				// app=tag-sandbox,tx=rw1,h=f0e76052
+				// request_tag: app=tag-sandbox,tx=rw1,h=f0e76052
 				err := transaction.QueryWithHash(ctx, spanner.NewStatement("@{LOCK_SCANNED_RANGES=exclusive} SELECT FirstName, LastName FROM Singers")).Do(nop)
 				time.Sleep(1 * time.Second)
 				return err
@@ -72,10 +72,10 @@ func run(ctx context.Context) error {
 	})
 
 	grp.Go(func() error {
-		// app=tag-sandbox,tx=rw2
+		// transaction_tag: app=tag-sandbox,tx=rw2
 		_, err = client.ReadWriteTransactionTagString(ctx, "tx=rw2",
 			func(ctx context.Context, transaction *spannerplus.ReadWriteTransactionWrapper) error {
-				// app=tag-sandbox,tx=rw2,h=f0e76052
+				// request_tag: app=tag-sandbox,tx=rw2,h=f0e76052
 				err := transaction.QueryWithHash(ctx,
 					spanner.NewStatement("@{LOCK_SCANNED_RANGES=exclusive} SELECT FirstName, LastName FROM Singers")).Do(nop)
 				time.Sleep(1 * time.Second)
@@ -85,10 +85,10 @@ func run(ctx context.Context) error {
 	})
 
 	grp.Go(func() error {
-		// app=tag-sandbox,tx=rw3
+		// transaction_tag: app=tag-sandbox,tx=rw3
 		_, err = client.ReadWriteTransaction(ctx, []spannerplus.Tag{{"tx", "rw3"}},
 			func(ctx context.Context, transaction *spannerplus.ReadWriteTransactionWrapper) error {
-				// app=tag-sandbox,tx=rw3,h=69daf312
+				// request_tag: app=tag-sandbox,tx=rw3,h=69daf312
 				err := transaction.QueryWithHash(ctx,
 					spanner.NewStatement("@{LOCK_SCANNED_RANGES=exclusive} SELECT BirthDate FROM Singers")).Do(nop)
 				time.Sleep(1 * time.Second)
